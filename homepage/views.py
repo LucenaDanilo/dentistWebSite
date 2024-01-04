fe404fa11c62f35b92bef0411a78c91db39202c6
Lucena.danilo <danilo.lucena.ext@synchro.com.br>
2024-01-04 11:30:45 -0300

    create django project

---
+++ homepage/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def homepage_view(request):
+    return render(
+        request,
+        'cars.html',
+        {"cars" : 'oi'}
+    )
